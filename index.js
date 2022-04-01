7c1b02cb27b2edd25f8c3a7f0d8b1cc095199684
yellowww <78814395+yellowww@users.noreply.github.com>
2022-03-31 22:49:47 -0400

    re-include exception catch

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = require("./router.js");
 router.setupRouts();
 main.init();
 
-//process.on('uncaughtException', main.exitCleanup);
+process.on('uncaughtException', main.exitCleanup);
 
 
 
